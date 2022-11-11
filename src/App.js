b0fca9c090076e8468e055f6d381d94a0a1a6077
AMANLODHA <111699707+AMANLODHA@users.noreply.github.com>
2022-11-11 18:49:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Resturant from "./component/Basics/Resturant";
+//import UseState from "./component/Hooks/useState";
+//import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+import Todo from "./component/todoreact/Todo";
+//import Temp from "./component/weather/Temp";
+
+const App = () => {
+  return < Todo />;
+};
+
+export default App;
